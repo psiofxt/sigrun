7cfe76485513c2520618f3ccee291e770015edb4
Andrew <arellok@gmail.com>
2020-06-15 11:18:59 -0700

    remove unused imports

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,3 @@
-extern crate bitcoin;
-extern crate secp256k1;
-
-use bitcoin::network::constants::Network;
-use bitcoin::util::address::Address;
-use bitcoin::util::key;
-
-use hdwallet::{KeyChain, DefaultKeyChain, ExtendedPrivKey};
-use secp256k1::Secp256k1;
-
 mod wallets;
 use wallets::BitcoinWallet;
 
